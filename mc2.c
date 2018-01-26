b4a04247337496601dbad0edc301247591f68529
Andrew Johnston <ajohnston2@wpi.edu>
2018-01-25 20:45:56 -0500

    what i have

--- mc2.c
+++ mc2.c
@@ -1,6 +1,6 @@
 /*========Mid Day Commander, V2==========
 *=============Itteration: V2=============
-*==Authors: Matthew Hagan & Trevor Dowd==
+*====
 */
 //include statements
 
@@ -14,14 +14,17 @@
 #include <sys/resource.h>
 #include <sys/types.h>
 
-void processNumCommands(int cmd);
+void processBasicCmds(int cmd);
+void processCustomCommands(int cmd);
 void processCharCommands(char cmd);
 static int numID = 3;  // id of the command
 static int bCount = 0;
 
 struct bNode {
-  struct timeval start;
+  int jobNumber;
+  char *cmdName;
   int pid;
+  struct timeval start;
   int cmdID;
   struct bNode *next;
 };
@@ -40,42 +43,58 @@ struct cmdNode *cHead = NULL;
 struct bNode *bHead = NULL;
 
 // adds a process to the background process list
-void addBprocess(int pid, struct timeval start, int cmdID) {
+void addBprocess(int pid, struct cmdNode *n,struct timeval start) {
     struct bNode * current = bHead;
     while (current->next != NULL) {
         current = current->next;
     }//end of while
     current->next = malloc(sizeof(struct bNode));
     current->next->pid = pid;
-    current->next->cmdID = cmdID;
+    current->next->cmdID = n->id;
+    current->next->jobNumber = ++bCount;
+    current->next->cmdName = n->name;
     current->next->start = start;
     // set next to NULL
     current->next->next = NULL;
+    printf("--Command: %s --\n",current->next->cmdName);
+    printf("\n[%d]PID: %d",current->next->jobNumber,current->next->pid);
 }//end of addToList
 
-
-// removes a process to the background process list
-void removeBprocess(int gcPID) {
-    int found = 0;
-    struct bNode * current = bHead;
+struct bNode *getBprocess(int PID){
+  struct bNode * current = bHead;
     while (current->next != NULL) {
       current = current->next;
-      if(current->pid == gcPID){
-        found = 1;
+      if(current->pid == PID){
+        return current;
       }
-      // its in the middle
-      if(found && current->next != NULL){
-        current->pid = current->next->pid;
-        current->cmdID = current->next->cmdID;
-        current->start = current->next->start;
-        // current->next = current->next->next;
+    }
+    printf("process not found");
+    return NULL;
+}
+
+// removes a process to the background process list prints job number and PID stats
+void completeBprocess(int PID) {
+    int found = 0;
+    struct bNode * current = bHead;
+    struct bNode * trash = NULL;
+    while (current->next != NULL) {
+      if(current->next->pid == PID){
+        found++;
+        bCount--;
+        trash = current->next;
+        if(trash->next != NULL){
+          current->next = current->next->next;
+        }
+        printf("--Job Complete [%d] --\n",trash->jobNumber);
+        printf("Command Name:%s",trash->cmdName);
+        printf("Process ID: PID\n");
+        free(trash);
       }
-      // its at the end of the list
-      else if(found){
-        free(current);
-        current = NULL;
-      }
-    }//end of while
+      current->jobNumber-=found;
+      current = current->next;
+    }
+    if(!found)
+      printf("PID not in list\n");
 } // end of remove
 
 
@@ -83,14 +102,19 @@ void removeBprocess(int gcPID) {
 addToList function
   pushes an element onto a linked list for adding a command
 */    
-void addToList(int id, char *name, int length, int isBackground) {
+void addToList(int id, char *name, int length) {
     struct cmdNode * current = cHead;
     while (current->next != NULL) {
         current = current->next;
     }//end of while
     current->next = malloc(sizeof(struct cmdNode));
     current->next->id = id;
-    current->next->isBackground = isBackground;
+    current->next->isBackground = 0;
+    current->next->length = length;
+    if(name[length-1]=='&'){
+      printf("b\n");
+      current->next->isBackground = 1;
+    }
     current->next->name = (char *)malloc(sizeof(char *) * length);
     for(int i=0; i<length; i++){
       current->next->name[i] = name[i];
@@ -128,13 +152,7 @@ void executeCmd(struct cmdNode *current){
       token = strtok(NULL, " "); // advances to the next token
       i++;
   }
-  
-  if(current->isBackground == 1){
-    argv[i-1] = NULL;
-  } else{
-    argv[i] = NULL;
-  }
-  
+  argv[i-current->isBackground] = NULL;
   //command
   if(execvp(argv[0], argv) == -1)
   {
@@ -143,32 +161,6 @@ void executeCmd(struct cmdNode *current){
   }
 }
 
-void logOff(){
-  while(bCount > 0){
-    printf("Background Commands are still running\n");
-    wait(0);
-  }
-    printf("Logging you out, Commander\n");
-    deleteList();
-    exit(0);
-}
-
-int isBackGround(int cmd){
-  // returns boolean is command
-  struct cmdNode * current = cHead; //
-  while(current->next != NULL) 
-  {
-    if(current->next->isBackground){
-      return 1;
-    } else{
-      current = current->next;
-    }
-  } // end while
-  return 0;
-}
-
-
-
 void printStart(){
   //intial statements giving the commander options on what to do 
       printf("===== Mid-Day Commander, v2 =====\n G’day, Commander! What command would you like to run?");
@@ -176,7 +168,7 @@ void printStart(){
       printf("1. last : Prints out the result of the last command");
       printf("\n2. ls : Prints out the result of a listing on a user-specified path \n");
       struct cmdNode * current = cHead; //
-       while(current->next != NULL) 
+      while(current->next != NULL) 
       {
         current = current->next;
         printf("%d", current->id);
@@ -189,36 +181,65 @@ void printStart(){
       printf("e. exit : Leave Mid-Day Commander\n");
       printf("p. pwd : Prints working directory\n");
       printf("r. running processes : Prints a list of running processes\n");
-
 }
+
+
 
 void printBR(){
     struct bNode * current = bHead;
+    printf("\n");
     while (current->next != NULL) {
-      printf("fuckbois\n");
       current = current->next;
-      printf("\nPID: ");
-      printf("%d", current->pid);
+      printf("--Command: %s --\n",current->cmdName);
+      printf("\n[%d]PID: %d",current->jobNumber,current->pid);
     }
+    printf("\n");
 }
 
-void printBRComplete(int gcPID, struct timeval bpstart){
-  printf("Background Process Complete: \n");
-  printf("\nPID: ");
-  printf("%d\n", gcPID);
-  struct timeval bpfinish;
-  struct rusage bpPages;
-  gettimeofday(&bpfinish, 0);
-  getrusage(RUSAGE_SELF, &bpPages);
-  long bpPageFaults = bpPages.ru_majflt;
-  long bpPageFaultsReclaimed = bpPages.ru_minflt;
-  printf("-- Statistics --- \n Elapsed time:");
-  long time = (bpfinish.tv_sec-bpstart.tv_sec) + bpfinish.tv_usec-bpstart.tv_usec;//time value
+void runStats(struct timeval tstart){
+  struct timeval tfinish;
+  struct rusage pages;
+  // wait command for process holding
+  gettimeofday(&tfinish, 0);
+  getrusage(RUSAGE_SELF, &pages);
+  long pageFaults = pages.ru_majflt;
+  long pageFaultsReclaimed = pages.ru_minflt;
+  printf("== Statistics == \n Elapsed time:");
+  long time = (tfinish.tv_sec-tstart.tv_sec) + tfinish.tv_usec-tstart.tv_usec;//time value
   printf("%ld", time);
   printf(" milliseconds \n Page Faults: ");
-  printf("%ld", bpPageFaults);
+  printf("%ld", pageFaults);
   printf("\n Page Faults (reclaimed): ");
-  printf("%ld \n", bpPageFaultsReclaimed);
+  printf("%ld \n", pageFaultsReclaimed);
+  //get reclaimed page faults 
+}
+
+void runBStats(int PID){
+  struct bNode *process = getBprocess(PID);
+  if(process){
+    completeBprocess(PID);
+    runStats(process->start);
+  }
+}
+
+void logOff(){
+  while(bCount > 0){
+    printf("Background Commands are still running\n");
+    runBStats(wait(0));
+  }
+  printf("Logging you out, Commander\n");
+  deleteList();
+  exit(0);
+}
+
+void purgeBList(){
+  int status;
+  for(int i = 0; i < bCount;i++){
+    int PID = waitpid(0,&status,WNOHANG);
+    if(PID){
+      runBStats(PID);
+    }
+  }
 }
 
 int main(){
@@ -228,48 +249,51 @@ int main(){
   
   while(1){
     fflush(stdin);
+    int status;
+    purgeBList();
     printStart();
-    char *optn = malloc(sizeof(char)*80);
+    char *optn = NULL;
     size_t buffer = 80;
     int na = getline(&optn, &buffer, stdin);
+    purgeBList();
     if(na ==-1)
         logOff();
-      optn[na-1] = '\0';
-      char order = optn[0];
-      int numOrder;
-      if(isdigit(order)){
+    optn[na-1] = '\0';
+    char order = optn[0];
+    int numOrder;
+    if(isdigit(order)){
         numOrder = atoi(optn);
-        free(optn);
-        if(numOrder<numID)
-          processNumCommands(numOrder);
-        else
+        if(numOrder<numID){
+          if(numOrder<3)
+            processBasicCmds(numOrder);
+          else
+            processCustomCommands(numOrder);
+        }
+        else{
           printf("This option is currently not available. Please try again.\n");
+        }
       }
       else{
-        free(optn);
         processCharCommands(order);
       }
-      
+      free(optn);
 
-      
-    
   } // while loop
   return 0;
-
 } // main
 
 void processCharCommands(char order){
   switch(order){
         case 'a':
           printf("Command to Add:");
-          char *cmd = malloc(sizeof(char)*128);
-          size_t buffer = 128;
+          char *cmd = NULL;
+          size_t buffer = 140;
           int na = getline(&cmd, &buffer, stdin);
           if(na == -1)
             logOff();
           cmd[na-1] = '\0';
           //add the command to list
-          addToList(numID, cmd, strlen(cmd),cmd[na-2]=='&');
+          addToList(numID, cmd, strlen(cmd));
           printf("Okay commander, added with ID: %d\n", numID);
           numID++;//increments numID
           free(cmd);
@@ -277,7 +301,7 @@ void processCharCommands(char order){
         case 'c':
            //fflush(stdin);
           printf("--Change Directory--\nNew Directory?:");
-          char *c = malloc(sizeof(char)*80);
+          char *c = NULL;
           size_t buf = 80;
           int na2 = getline(&c, &buf, stdin);
           if(na2 == -1)
@@ -297,6 +321,7 @@ void processCharCommands(char order){
           break;
         case 'r':
           printf("--Background Processes Running--\n");
+          purgeBList();
           printBR();
           break;
         default:
@@ -306,7 +331,63 @@ void processCharCommands(char order){
   printf("\n");// seperating midday output
 }
 
-void processNumCommands(int order){
+//v0 functionality
+void processBasicCmds(int order){
+  struct timeval tstart;//struct data for time
+  if(fork())
+    runStats(tstart);
+  else{
+    char *cmd;
+    switch(order){
+      case 0:
+        printf("-- Who Am I? -- \n");
+        //command parts
+        cmd = "whoami";
+        //command
+        execvp(cmd, &cmd);
+        break;
+
+      case 1:
+        printf("-- Last Logins -- \n");
+        //command parts
+        cmd = "last";
+        //command
+        execvp(cmd, &cmd); 
+        break;
+      case 2:
+        printf("== Directory Listings == \n");
+        //command parts
+        char *ext = NULL;
+        char *path = NULL;
+        printf("\nArguments?: ");
+        size_t buffer = 80;
+        int na = getline(&ext, &buffer, stdin);
+        if(na == -1)
+          logOff();
+        ext[na-1] = '\0';
+        printf("\nPath?: ");
+        size_t buffer2 = 80;
+        int na2 = getline(&path, &buffer2, stdin);
+        if(na2 == -1)
+          logOff();
+        path[na2-1] = '\0';
+        cmd = "ls";
+        char *argv[4];
+        argv[0] = "ls";
+        argv[1] = ext;
+        argv[2] = path;
+        argv[3] = NULL;
+        //command
+        execvp(cmd, argv);
+        free(ext);
+        free(path);
+        break;
+    }
+  }
+}
+
+void processCustomCommands(int order){
+  struct timeval tstart;//struct data for time
   struct cmdNode * current = cHead;
   int pos = order - 2;
   // get the position of the command in the LL
@@ -315,132 +396,21 @@ void processNumCommands(int order){
     current = current->next;
     i++;
   }
-  struct timeval tstart, tfinish;//struct data for time
-  struct rusage pages;//struct data for usage
-  int cPID;
+  int cPID = fork();
   gettimeofday(&tstart, 0);
-  if((current->isBackground==0)  && ((cPID = fork()) != 0)){ // not backgroud process
-        if(cPID)// parent
-        {
-          
-          
-          wait(0);// wait command for process holding
-          gettimeofday(&tfinish, 0);
-          getrusage(RUSAGE_SELF, &pages);
-          long pageFaults = pages.ru_majflt;
-          long pageFaultsReclaimed = pages.ru_minflt;
-          printf("-- Statistics --- \n Elapsed time:");
-          long time = (tfinish.tv_sec-tstart.tv_sec) + tfinish.tv_usec-tstart.tv_usec;//time value
-          printf("%ld", time);
-          printf(" milliseconds \n Page Faults: ");
-          printf("%ld", pageFaults);
-          printf("\n Page Faults (reclaimed): ");
-          printf("%ld \n", pageFaultsReclaimed);
-          //get reclaimed page faults
-        } else {// child
-             
-            if(order== 0)//who am i command
-            {
-              
-              printf("-- Who Am I? -- \n");
-
-              //command parts
-              char *cmd = "whoami";
-              char *argv[2];
-              argv[0] = "whoami";
-              argv[1] = NULL;
-
-              //command
-              execvp(cmd, argv);
-
-            }
-            else if(order== 1)//last command
-            {
-              printf("-- Last Logins -- \n");
-
-              //command parts
-              char *cmd = "last";
-              char *argv[2];
-              argv[0] = "last";
-              argv[1] = NULL;
-
-              //command
-              execvp(cmd, argv); 
-            }
-            else if(order==2)//directory listing command
-            {
-              printf("-- Directory Listings -- \n");
-
-              //command parts
-              char *ext = malloc(sizeof(char)*80);
-              char *path = malloc(sizeof(char)*80);
-              
-              printf("\nArguments?: ");
-              size_t buffer = 80;
-              int na = getline(&ext, &buffer, stdin);
-              if(na == -1)
-                logOff();
-              ext[na-1] = '\0';
-              
-              printf("\nPath?: ");
-              size_t buffer2 = 80;
-              int na2 = getline(&path, &buffer2, stdin);
-              if(na2 == -1)
-                logOff();
-              path[na2-1] = '\0';
-                          
-              
-              char *cmd = "ls";
-              char *argv[4];
-              argv[0] = "ls";
-              argv[1] = ext;
-              argv[2] = path;
-              argv[3] = NULL;
-              
-              //command
-              execvp(cmd, argv);
-              free(ext);
-              free(path);
-            }//else if 
-            else{ 
-              // execute command
-              executeCmd(current);
-            }//else
-        }
- } else{ // background process
-      if(cPID)
-      {
-        int statusParent;
-        struct rusage brusageParent;
-        wait3(&statusParent, WNOHANG, &brusageParent);
-      } // if
-      else if(cPID == 0) // child
-      {
-        int status;
-        struct rusage brusage;
-        struct timeval bpstartTime;
-        int gcPID;
-        if((gcPID = fork()) != 0){  // child
-          addBprocess(gcPID,tstart,current->id);
-          gettimeofday(&bpstartTime, 0);
-          bCount++;
-          printf("-- Command: %s --", current->name);
-          printf("[%d] %d /* indicate background task #1 and process id */", bCount, gcPID);
-          printf("\n first motherfuckers\n");
-          wait3(&status, 0, &brusage);  // wait for grandchild process to finish executing
-          printf("\n third better this be\n");
-          printBRComplete(gcPID, bpstartTime);
-          bCount = bCount - 1;
-          removeBprocess(gcPID);
-          exit(0);
-
-        } // end if
-        else { // grandchild
-          printf("\nthis better be second\n");
-          executeCmd(current);
-   // running in background
-        } // end else
-      } // else if
-    } // end else
-}
+   // not backgroud process
+  if(cPID)// parent
+  {
+    if(current->isBackground==0){
+      wait(0);
+      runStats(tstart);
+    }
+    else{
+      addBprocess(cPID,current,tstart);
+    } 
+  } else {// child
+        executeCmd(current);
+  }
+  
+} 
         
